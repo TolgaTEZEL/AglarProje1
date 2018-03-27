532b529ecd560610ece1137729c914f91be2cdd6
Tolga Tezel <ttezel46@gmail.com>
2018-03-27 16:37:17 +0300

    Ağlar Projesi commit 3
    salı 4.30 da atılan yeni commit. Sınıflar geliştirildi Proje 2 proje olarak ayrıldı bir kaç ekleme yapıldı.

---
+++ Con4Server/src/game/Message.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+import static game.Message.Message_Type.None;
+/**
+ *
+ * @author Tolga
+ */
+public class Message {
+     public static enum Message_Type {None, Name, Disconnect,RivalConnected, Text, Selected, Finish,Start,}
+    
+    public Message_Type type;
+    public Object content;
+    public Message(Message_Type t)
+    {
+        this.type=t;
+    }
+}
